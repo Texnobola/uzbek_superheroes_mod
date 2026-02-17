3ab332e70e12b07fa19a0649dadcffc5f301970a
Texnobola <{texnobola2@gmail.com}>
2026-02-17 12:15:29 +0500

    tiny-fix

--- src/main/java/net/mcreator/uzbekqahramonlar/item/LaserSaberItem.java
+++ src/main/java/net/mcreator/uzbekqahramonlar/item/LaserSaberItem.java
@@ -18,11 +18,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 
 import net.mcreator.uzbekqahramonlar.procedures.LaserSaberItemInHandTickProcedure;
+import net.mcreator.uzbekqahramonlar.procedures.LaserSaberHitSoundProcedure;
 import net.mcreator.uzbekqahramonlar.item.renderer.LaserSaberItemRenderer;
 
 import java.util.function.Consumer;
@@ -106,6 +108,13 @@ public class LaserSaberItem extends Item implements GeoItem {
 			return builder.build();
 		}
 		return super.getDefaultAttributeModifiers(equipmentSlot);
+	}
+
+	@Override
+	public boolean onEntitySwing(ItemStack itemstack, LivingEntity entity) {
+		boolean retval = super.onEntitySwing(itemstack, entity);
+		LaserSaberHitSoundProcedure.execute();
+		return retval;
 	}
 
 	@Override
