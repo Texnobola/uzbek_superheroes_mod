a080ffb047a9a1a2396b0a32132aecd47be50a3c
Texnobola <{texnobola2@gmail.com}>
2026-02-17 20:27:48 +0500

    now check

--- src/main/java/net/mcreator/uzbekqahramonlar/procedures/LaserSaberCombatHitProcedureProcedure.java
+++ src/main/java/net/mcreator/uzbekqahramonlar/procedures/LaserSaberCombatHitProcedureProcedure.java
@@ -1,6 +1,20 @@
 package net.mcreator.uzbekqahramonlar.procedures;
 
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.particles.ParticleTypes;
+
 public class LaserSaberCombatHitProcedureProcedure {
-	public static void execute() {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
+		if (entity == null || sourceentity == null) return;
+		ItemStack itemstack = (sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY);
+		if (!itemstack.getOrCreateTag().getBoolean("is_on")) return;
+		entity.setSecondsOnFire(5);
+		if (world instanceof ServerLevel _level) {
+			_level.sendParticles(ParticleTypes.ELECTRIC_SPARK, x, y + 1, z, 15, 0.5, 0.5, 0.5, 0.1);
+		}
 	}
 }
--- src/main/java/net/mcreator/uzbekqahramonlar/procedures/LaserSaberTreeChopProcedureProcedure.java
+++ src/main/java/net/mcreator/uzbekqahramonlar/procedures/LaserSaberTreeChopProcedureProcedure.java
@@ -1,6 +1,31 @@
 package net.mcreator.uzbekqahramonlar.procedures;
 
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.uzbekqahramonlar.init.UzbekQahramonlarModItems;
+
 public class LaserSaberTreeChopProcedureProcedure {
-	public static void execute() {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null) return;
+		ItemStack itemstack = (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY);
+		if (itemstack.getItem() != UzbekQahramonlarModItems.LASER_SABER.get() || !itemstack.getOrCreateTag().getBoolean("is_on")) return;
+		BlockPos startPos = BlockPos.containing(x, y, z);
+		if (world.getBlockState(startPos).is(BlockTags.LOGS)) {
+			for (int h = 0; h <= 35; h++) {
+				for (int dx = -3; dx <= 3; dx++) {
+					for (int dz = -3; dz <= 3; dz++) {
+						BlockPos currentPos = startPos.offset(dx, h, dz);
+						BlockState state = world.getBlockState(currentPos);
+						if (state.is(BlockTags.LOGS) || state.is(BlockTags.LEAVES)) world.destroyBlock(currentPos, true);
+					}
+				}
+			}
+		}
 	}
 }
